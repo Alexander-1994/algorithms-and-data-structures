e5285c95a74f3138a128a7f3d46d9db5a16c0917
Alexander <kisliy.doma@gmail.com>
2025-12-21 23:12:55 +0800

    homework

---
+++ Part 4. Data structures/queue.js
@@ -0,0 +1,51 @@
+class Queue {
+  constructor() {
+    this.items = [];
+  }
+
+  offer(element) {
+    this.items.push(element);
+  }
+
+  poll() {
+    if (this.isEmpty()) {
+      throw new Error("Queue is empty");
+    }
+
+    return this.items.shift();
+  }
+
+  peek() {
+    if (this.isEmpty()) {
+      throw new Error("Queue is empty");
+    }
+
+    return this.items[0];
+  }
+
+  isEmpty() {
+    return this.items.length === 0;
+  }
+
+  size() {
+    return this.items.length;
+  }
+
+  clear() {
+    this.items = [];
+  }
+
+  print() {
+    console.log(this.items.join(", "));
+  }
+}
+
+const queue = new Queue();
+
+queue.offer("A");
+queue.offer("B");
+queue.offer("C");
+console.log(queue.poll()); // A
+queue.offer("D");
+console.log(queue.peek()); // B
+queue.print(); // B, C, D
---
+++ Part 4. Data structures/stack.js
@@ -0,0 +1,49 @@
+class Stack {
+  constructor() {
+    this.items = [];
+  }
+
+  push(element) {
+    this.items.push(element);
+  }
+
+  pop() {
+    if (this.isEmpty()) {
+      throw new Error("Stack is empty");
+    }
+    return this.items.pop();
+  }
+
+  peek() {
+    if (this.isEmpty()) {
+      throw new Error("Stack is empty");
+    }
+    return this.items[this.items.length - 1];
+  }
+
+  isEmpty() {
+    return this.items.length === 0;
+  }
+
+  size() {
+    return this.items.length;
+  }
+
+  clear() {
+    this.items = [];
+  }
+
+  print() {
+    console.log(this.items.join(", "));
+  }
+}
+
+const stack = new Stack();
+
+stack.push(1);
+stack.push(2);
+stack.push(3);
+stack.pop();
+stack.push(4);
+console.log(stack.peek()); // 4
+stack.print(); // 1, 2, 4
